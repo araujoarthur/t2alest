fcce554f5bc9071288cb7082a659693176824370
Arthur Galdino <28057772+araujoarthur@users.noreply.github.com>
2025-06-18 01:42:25 -0300

    Added some documentations to guide me through implementations later today

--- tree/tree.go
+++ tree/tree.go
@@ -18,7 +18,25 @@ func (t *Tree) Root() *FolderNode {
 	return &t.root
 }
 
-func (t *Tree) CreateFile(path string, name string) (*FileNode, error)                     {}
+/*
+Follows a path and return the final node or an error if it's not possible to follow through the end.
+*/
+func (t *Tree) followPath(path string) (*Node, error) {}
+
+/*
+Follows a path up to the point it's not possible anymore (i.e the rest of the path doesn't exist). Returns the most distant existent element
+in the path.
+*/
+func (t *Tree) explorePath(path string) *Node {}
+
+/*
+Creates a file node at the given path.
+*/
+func (t *Tree) CreateFile(path string, name string) (*FileNode, error) {}
+
+/*
+Creates a folder at a given path. If recursive is false, the function will fail if any of the path's folders but the last does not exist.
+*/
 func (t *Tree) CreateFolder(path string, name string, recursive bool) (*FolderNode, error) {}
 func (t *Tree) RemoveFile(path string) error                                               { return nil }
 func (t *Tree) RemoveFolder(path string, recursive bool) error                             { return nil }
